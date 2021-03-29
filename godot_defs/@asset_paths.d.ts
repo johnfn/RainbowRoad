5d5f9e13f27b852daa0d235383cf1d3abbd44d47
Mike Precup <mike@precup.email>
2021-03-28 19:39:22 -0700

    Block coloring and scaling

--- godot_defs/@asset_paths.d.ts
+++ godot_defs/@asset_paths.d.ts
@@ -1,17 +1,21 @@
 
 declare type AssetType = {
-  'res://Player.tscn': import('/Users/johnfn/code/RainbowRoad/src/Player').Player,
+  'res://Player.tscn': import('/home/arch/RainbowRoad/src/Player').Player,
+  'res://RainbowRoad.png': StreamTexture,
+  'res://favicon.png': StreamTexture,
   'res://icon.png': StreamTexture,
+  'res://test.tscn': Node2D,
   'res://assets/block.png': StreamTexture,
   'res://assets/block1.png': StreamTexture,
   'res://assets/star-base.png': StreamTexture,
   'res://assets/star-glasses.png': StreamTexture,
-  'res://RainbowRoad.png': StreamTexture,
-  'res://favicon.png': StreamTexture,
-  'res://block.tscn': Sprite,
+  'res://compiled/Player.gd': import('/home/arch/RainbowRoad/src/Player').Player,
+  'res://compiled/Player.gd': import('/home/arch/RainbowRoad/src/Player').Player,
   'res://MainScene.tscn': Node2D,
-  'res://compiled/Player.gd': import('/Users/johnfn/code/RainbowRoad/src/Player').Player,
-  'res://compiled/Player.gd': import('/Users/johnfn/code/RainbowRoad/src/Player').Player
+  'res://blocktest.tscn': import('/home/arch/RainbowRoad/src/Block').Block,
+  'res://block.tscn': import('/home/arch/RainbowRoad/src/Block').Block,
+  'res://compiled/Block.gd': import('/home/arch/RainbowRoad/src/Block').Block,
+  'res://compiled/Block.gd': import('/home/arch/RainbowRoad/src/Block').Block
 }
 
 declare type AssetPath = keyof AssetType;
--- src/Block.ts
+++ src/Block.ts
@@ -1,19 +1,32 @@
 export class Block extends StaticBody2D {
-    beat: float;
-    x: float;
-    y: float;
-    width: float;
-    height: float;
+    colors = [Color(0, 0, 1), Color(1, 0, 1), Color(1, 0, 0), Color(1, 1, 0), Color(0, 1, 0), Color(0, 1, 1)]
+    beat: int = 1
+    curr_beat: float = 0.0
 
-    constructor(beat: float, x: float, y: float, width: float, height: float) {
-        this.beat = beat;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+    scale_curve(proximity: float) {
+        proximity /= 1.5 // range of 3 beats since it's double sided
+        return max(0, 1 - proximity)
     }
 
-    render() {
-        
+    _ready() {
+        let sprite = this.get_child(1)
+        sprite.modulate = this.colors[this.beat]
+    }
+
+    _physics_process(delta: float) { 
+        this.curr_beat += delta
+        while (this.curr_beat > this.colors.size()) {
+            this.curr_beat -= this.colors.size()
+        }
+        let beat_dist = abs(this.curr_beat - this.beat)
+        if (beat_dist > this.colors.size() / 2) {
+            beat_dist = this.colors.size() - beat_dist
+        }
+        let scale_factor = this.scale_curve(beat_dist)
+        let shape = this.get_child(0)
+        let sprite = this.get_child(1)
+        this.scale = Vector2(scale_factor, scale_factor)
+        sprite.scale = Vector2(scale_factor, scale_factor)
+        shape.scale = Vector2(scale_factor, scale_factor)
     }
 }
