304f702bd31904b5743b1f722bb80b936a1cd652
Mike Precup <mike@precup.email>
2021-03-28 21:39:39 -0700

    ur the star of the party

--- src/Block.ts
+++ src/Block.ts
@@ -1,41 +1,33 @@
 export class Block extends StaticBody2D {
-  colors = [
-    new Color(0, 0, 1),
-    new Color(1, 0, 1),
-    new Color(1, 0, 0),
-    new Color(1, 1, 0),
-    new Color(0, 1, 0),
-    new Color(0, 1, 1),
-  ]
-  curr_beat: float = 0.0
+  curr_beat: float = 0.0;
   @exports
-  beat_count: int = 1
+  beat_count: int = 1;
 
   scale_curve(proximity: float) {
-    proximity /= 1.5 // range of 3 beats since it's double sided
-    return max(0.0, 1 - proximity)
+    proximity /= 1.5; // range of 3 beats since it's double sided
+    return max(0.0, 1 - proximity);
   }
 
   _ready() {
-    let sprite = this.get_child(1)
-    sprite.modulate = this.colors[this.beat_count]
+    let sprite = this.get_child(1);
+    sprite.modulate = ColorSet.colors[this.beat_count];
   }
 
   _physics_process(delta: float) {
-    this.curr_beat += delta
-    while (this.curr_beat > this.colors.size()) {
-      this.curr_beat -= this.colors.size()
+    this.curr_beat += delta;
+    while (this.curr_beat > ColorSet.colors.size()) {
+      this.curr_beat -= ColorSet.colors.size();
     }
-    let beat_dist = abs(this.curr_beat - this.beat_count)
-    if (beat_dist > this.colors.size() / 2) {
-      beat_dist = this.colors.size() - beat_dist
+    let beat_dist = abs(this.curr_beat - this.beat_count);
+    if (beat_dist > ColorSet.colors.size() / 2) {
+      beat_dist = ColorSet.colors.size() - beat_dist;
     }
-    let scale_factor = this.scale_curve(beat_dist)
-    let shape = this.get_child(0)
-    let sprite = this.get_child(1)
-    this.scale = new Vector2(scale_factor, scale_factor)
-    sprite.scale = new Vector2(scale_factor, scale_factor)
-    shape.scale = new Vector2(scale_factor, scale_factor)
-    shape.disabled = scale_factor === 0.0
+    let scale_factor = this.scale_curve(beat_dist);
+    let shape = this.get_child(0);
+    let sprite = this.get_child(1);
+    this.scale = new Vector2(scale_factor, scale_factor);
+    sprite.scale = new Vector2(scale_factor, scale_factor);
+    shape.scale = new Vector2(scale_factor, scale_factor);
+    shape.disabled = scale_factor === 0.0;
   }
 }
--- src/Player.ts
+++ src/Player.ts
@@ -3,67 +3,91 @@
 
 export class Player extends KinematicBody2D {
   @exports
-  speed: float = 200.0
-  vy: float = 0.0
+  speed: float = 200.0;
+  vy: float = 0.0;
+  curr_beat: float = 0.0;
 
   constructor() {
-    super()
+    super();
+  }
+
+  set_eye_color(delta: float) {
+    this.curr_beat += delta;
+    while (this.curr_beat > ColorSet.colors.size()) {
+      this.curr_beat -= ColorSet.colors.size();
+    }
+    let color1i = floor(this.curr_beat);
+    let color_mix_perc = this.curr_beat - color1i;
+    let color2i = color1i + 1;
+    if (color2i == ColorSet.colors.size()) {
+      color2i--;
+    }
+    let color1 = ColorSet.colors[color1i];
+    let color2 = ColorSet.colors[color2i];
+
+    let h = color1.h * (1 - color_mix_perc) + color2.h * color_mix_perc;
+    let s = color1.s * (1 - color_mix_perc) + color2.s * color_mix_perc;
+    let v = color1.v * (1 - color_mix_perc) + color2.v * color_mix_perc;
+    let eye_color = Color.from_hsv(h, s, v);
+    this.get_node("Glasses").modulate = eye_color;
   }
 
   _physics_process(delta: float) {
-    let dx = 0.0
-    let dy = 0.0
+    this.set_eye_color(delta);
+
+    let dx = 0.0;
+    let dy = 0.0;
 
     if (!this.is_on_floor()) {
-      this.vy += 0.03
+      this.vy += 0.03;
     } else {
-      this.vy = 0
+      this.vy = 0;
     }
 
     if (Input.is_key_pressed(KeyList.KEY_A)) {
-      dx -= 1
+      dx -= 1;
     }
 
     if (Input.is_key_pressed(KeyList.KEY_D)) {
-      dx += 1
+      dx += 1;
     }
 
     if (Input.is_key_pressed(KeyList.KEY_W)) {
-      dy -= 1
+      dy -= 1;
     }
 
     if (Input.is_key_pressed(KeyList.KEY_S)) {
-      dy += 1
+      dy += 1;
     }
 
     if (Input.is_key_pressed(KeyList.KEY_SPACE) && this.is_on_floor()) {
-      this.vy = -1.3
+      this.vy = -1.3;
     }
 
     if (!Input.is_key_pressed(KeyList.KEY_SPACE)) {
       if (this.vy < 0) {
-        this.vy = 0
+        this.vy = 0;
       }
     }
 
-    dy += this.vy
+    dy += this.vy;
 
-    const dPosition = new Vector2(dx, dy).mul(this.speed)
+    const dPosition = new Vector2(dx, dy).mul(this.speed);
 
-    this.move_and_slide(dPosition, new Vector2(0, -1))
+    this.move_and_slide(dPosition, new Vector2(0, -1));
   }
 
   enter_door() {
-    let current_scene = this.get_tree().current_scene.name
+    let current_scene = this.get_tree().current_scene.name;
 
     if (current_scene === "Level1") {
-      this.get_tree().change_scene("res://Level2.tscn")
+      this.get_tree().change_scene("res://Level2.tscn");
     } else if (current_scene === "Level2") {
-      this.get_tree().change_scene("res://Level3.tscn")
+      this.get_tree().change_scene("res://Level3.tscn");
     } else if (current_scene === "Level3") {
-      this.get_tree().change_scene("res://YouWin.tscn")
+      this.get_tree().change_scene("res://YouWin.tscn");
     } else {
-      print("What the...")
+      print("What the...");
     }
   }
 }
