3ab507b4e71e4793df1f1a8121157cef9196a93a
Mike Precup <mike@precup.email>
2021-03-28 21:45:53 -0700

    woops missed a file

---
+++ src/ColorSet.ts
@@ -0,0 +1,17 @@
+class ColorSetSingleton extends Node2D {
+  public colors: Array<Color> = [
+    new Color(0, 0, 1),
+    new Color(1, 0, 1),
+    new Color(1, 0, 0),
+    new Color(1, 1, 0),
+    new Color(0, 1, 0),
+    new Color(0, 1, 1),
+  ];
+}
+
+export const ColorSet = new ColorSetSingleton();
+
+// _ready() {
+//   const root = this.get_tree().root;
+//   const current_scene = root.get_child(root.get_child_count() - 1);
+// }
