9cf86377072a148f67aaa5d3e6a0a5b65c1e522a
johnfn <johnfn@gmail.com>
2021-03-30 10:52:21 -0700

    Fix

--- src/Block.ts
+++ src/Block.ts
@@ -17,9 +17,11 @@ export class Block extends StaticBody2D {
 
   _physics_process(delta: float) {
     this.curr_beat += delta
+
     while (this.curr_beat >= ColorSet.colors.size()) {
       this.curr_beat -= ColorSet.colors.size()
     }
+
     let beat_dist = abs(this.curr_beat - this.beat_count)
     if (beat_dist > ColorSet.colors.size() / 2) {
       beat_dist = ColorSet.colors.size() - beat_dist
